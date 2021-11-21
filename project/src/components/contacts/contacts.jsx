1e1cf41a7e8e8b2bab2ced277f1e0780b3e625fd
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-11-21 23:33:03 +0300

    стилизация блока контакты

--- project/src/components/contacts/contacts.jsx
+++ project/src/components/contacts/contacts.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 function Contacts() {
   return (
-    <div className="contscts">
-      contacts
+    <div className="contacts">
+      <ul className="contacts__info">
+        <li className="contacts__item">
+          <h4 className="contacts__item-title">Адрес</h4>
+          <span className="contacts__item-text">Санкт-Петербург,<br/>набережная реки Карповки, дом 5</span>
+        </li>
+        <li className="contacts__item">
+          <h4 className="contacts__item-title">Режим работы</h4>
+          <span className="contacts__item-text">Ежедневно, с 10:00 до 21:00</span>
+        </li>
+        <li className="contacts__item">
+          <h4 className="contacts__item-title">Телефон</h4>
+          <a className="contacts__item-text" href="tel:+78003335599">8 (800) 333-55-99</a>
+        </li>
+        <li className="contacts__item">
+          <h4 className="contacts__item-title">E-mail</h4>
+          <a className="contacts__item-text" href="mailto:info@avto-moto.ru">info@avto-moto.ru</a>
+        </li>
+      </ul>
+      <div className="map"></div>
     </div>
   );
 }
