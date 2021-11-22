a56e81def45093a89b989342c032cfd3ac9106fd
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-11-22 23:35:23 +0300

    разметка и стилизация footer

--- project/src/components/site-page/site-page.jsx
+++ project/src/components/site-page/site-page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../logo/logo';
 import MainNavigation from '../main-navigation/main-navigation';
 import AutoCard from '../auto-card/auto-card';
+import Footer from '../footer/footer';
 
 function SitePage() {
   return (
@@ -15,6 +16,9 @@ function SitePage() {
       <main>
         <AutoCard/>
       </main>
+      <div className="wrapper">
+        <Footer/>
+      </div>
     </>
   );
 }
