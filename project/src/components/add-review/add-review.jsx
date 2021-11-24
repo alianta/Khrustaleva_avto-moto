65017aafc1e8849a221e87aaec35b33bc1760d96
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-11-24 22:17:11 +0300

    сохранение данных формы в localStorage

--- project/src/components/add-review/add-review.jsx
+++ project/src/components/add-review/add-review.jsx
@@ -2,12 +2,27 @@ import React, {useState} from 'react';
 
 const RATING_STAR_COUNT = 5;
 
+
 function AddReview() {
   const [rating, setRating] = useState(0);
   const [popUpStatus, setPopUpStatus] = useState(0);
+  const [name, setName] = useState('');
+  const [pros, setPros] = useState('');
+  const [cons, setCons] = useState('');
+  const [comment, setComment] = useState('');
+
+  const onFormSubmit = (evt) => {
+    evt.preventDefault();
+    localStorage.setItem('name', name);
+    localStorage.setItem('pros', pros);
+    localStorage.setItem('cons', cons);
+    localStorage.setItem('rating', rating);
+    localStorage.setItem('comment', comment);
+  };
+
   return (
-    <div className={`pop-up ${popUpStatus?'pop-up--close':''}`}>
-      <form id="add-review-form" className="review-form" action="https://echo.htmlacademy.ru/" method="get">
+    <div className={`pop-up ${popUpStatus?'pop-up--close':''}`} >
+      <form id="add-review-form" className="review-form" onSubmit={onFormSubmit}>
         <button
           className="pop-up__close-button"
           type="button"
@@ -17,15 +32,15 @@ function AddReview() {
         <h2 className="review-form__title">Оставить отзыв</h2>
         <div className="review-form__field required">
           <label className="review-form__label visually-hidden" htmlFor="review-form__name">Имя</label>
-          <input id="review-form__name" className="review-form__input" type="text" name="name" placeholder="Имя"/>
+          <input id="review-form__name" className="review-form__input" type="text" name="name" placeholder="Имя" onChange={(evt) => setName(evt.target.value)}/>
         </div>
         <div className="review-form__field">
           <label className="review-form__label visually-hidden" htmlFor="review-form__pros">Достоинства</label>
-          <input id="review-form__pros" className="review-form__input" type="text" name="pros" placeholder="Достоинства"/>
+          <input id="review-form__pros" className="review-form__input" type="text" name="pros" placeholder="Достоинства" onChange={(evt) => setPros(evt.target.value)}/>
         </div>
         <div className="review-form__field">
           <label className="review-form__label visually-hidden" htmlFor="review-form__cons">Недостатки</label>
-          <input id="review-form__cons" className="review-form__input" type="text" name="cons" placeholder="Недостатки"/>
+          <input id="review-form__cons" className="review-form__input" type="text" name="cons" placeholder="Недостатки" onChange={(evt) => setCons(evt.target.value)}/>
         </div>
         <div className="review-form__field">
           <span className="review-form__label">Оцените товар:</span>
@@ -43,7 +58,6 @@ function AddReview() {
                       value={`${id}`}
                       onChange={({target}) => {
                         setRating(+target.value+1);
-                        /*setSubmitButtonDisabled(getSubmitButtonStatus(target.value, review.length));*/
                       }}
                     />
                     <label className={`rating__label ${rating>=id+1?'rating__label--active':''}`} htmlFor={`star-${id}`}></label>
@@ -55,7 +69,7 @@ function AddReview() {
         </div>
         <div className="review-form__field  review-form__field--last required">
           <label className="review-form__label visually-hidden" htmlFor="review-form__comment">Комментарий</label>
-          <textarea id="review-form__comment" className="review-form__comment"  name="comment" placeholder="Комментарий"></textarea>
+          <textarea id="review-form__comment" className="review-form__comment"  name="comment" placeholder="Комментарий" onChange={(evt) => setComment(evt.target.value)}></textarea>
         </div>
         <button className="review-form__submit" type="submit">Оставить отзыв</button>
       </form>
