372bd72fa41c201246e8cbe8d79b253cf5d6e55d
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-11-26 00:36:54 +0300

    статусы рейтинга в отзывах

--- project/src/components/reviews/reviews.jsx
+++ project/src/components/reviews/reviews.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {POPUP_CLOSE_BUTTON_ID} from '../../const';
+import {POPUP_CLOSE_BUTTON_ID, RatingNames, RATING_STAR_COUNT} from '../../const';
+
+const RATING_TEMPLATE_VALIE = 3;
 
 function Reviews() {
   return (
@@ -18,7 +20,15 @@ function Reviews() {
             <h3 className="comment__title">Комментарий</h3>
             <p className="comment__text">Взяли по трейд-ин, на выгодных условиях у дилера. Стильная внешка и крут по базовым характеристикам. Не думал, что пересяду на китайский автопром, но сейчас гоняю и понимаю, что полностью доволен.</p>
           </div>
-          <span className="review-rating review-rating--match">Советует</span>
+          <div className="rating-value">
+            {Array.from({length: RATING_STAR_COUNT}).map((_, id) => {
+              const keyValue = `rating-value-star-${id}`;
+              return (
+                <span key={keyValue} className={`rating-value__star ${RATING_TEMPLATE_VALIE>=id+1?'rating-value__star--active':''}`}></span>
+              );
+            })}
+            <span className="rating-value__name">Советует</span>
+          </div>
           <div className="review-footer">
             <time className="review-footer__date" dateTime="2021-11-22T18:18">1 минуту назад</time>
             <button className="review-footer__button">Ответить</button>
@@ -36,7 +46,15 @@ function Reviews() {
             <h3 className="comment__title">Комментарий</h3>
             <p className="comment__text">Дизайн отличный, управление просто шикарно, ощущения за рулём такой машины особые. Но ремонт очень дорогой. Пару месяцев назад пришлось менять двигатель. По стоимости вышло как новый автомобиль. Так что, если покупать эту машину, надо быть готовым к большим расходам на обслуживание.</p>
           </div>
-          <span className="review-rating review-rating--match">Советует</span>
+          <div className="rating-value">
+            {Array.from({length: RATING_STAR_COUNT}).map((_, id) => {
+              const keyValue = `rating-value-star-${id}`;
+              return (
+                <span key={keyValue} className={`rating-value__star ${RATING_TEMPLATE_VALIE>=id+1?'rating-value__star--active':''}`}></span>
+              );
+            })}
+            <span className="rating-value__name">Советует</span>
+          </div>
           <div className="review-footer">
             <time className="review-footer__date"  dateTime="2021-11-22T18:18">1 минуту назад</time>
             <button className="review-footer__button">Ответить</button>
@@ -55,7 +73,15 @@ function Reviews() {
               <h3 className="comment__title">Комментарий</h3>
               <p className="comment__text">{localStorage.getItem('comment')}</p>
             </div>
-            <span className="review-rating review-rating--match">Советует</span>
+            <div className="rating-value">
+              {Array.from({length: RATING_STAR_COUNT}).map((_, id) => {
+                const keyValue = `rating-value-star-${id}`;
+                return (
+                  <span key={keyValue} className={`rating-value__star ${localStorage.getItem('rating')>=id+1?'rating-value__star--active':''}`}></span>
+                );
+              })}
+              <span className="rating-value__name">{RatingNames[localStorage.getItem('rating')]}</span>
+            </div>
             <div className="review-footer">
               <time className="review-footer__date"  dateTime="2021-11-22T18:18">1 минуту назад</time>
               <button className="review-footer__button">Ответить</button>
